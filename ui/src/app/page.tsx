04eab0d0e4811e171cab5eb2667ce6cdf926832f
Lucas V. F. Ventura <lucasvfventura@gmail.com>
2025-04-24 20:30:11 -0700

    fixed style

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
 
   return (
     <div className="flex flex-col gap-6">
-      <h1 className="text-3xl font-bold mb-4">Rewards</h1>
+      <h1 className="text-2xl font-bold mb-4">Rewards</h1>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
         {data?.rewards?.map((reward) => (
           <div key={reward?.id} className="card bg-base-200 shadow-xl">
